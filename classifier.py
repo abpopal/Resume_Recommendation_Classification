9198a6046ff6eedb701223cf527ad71e9b09722e
abpopal <55827149+abpopal@users.noreply.github.com>
2021-11-19 17:35:01 +0430

    deleted unnecessary files
    Unnecessary files were deleted

--- classifier.py
+++ classifier.py
@@ -12,13 +12,20 @@ import pandas as pd
 import re
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.multiclass import OneVsRestClassifier
-
+from sklearn.metrics import accuracy_score, confusion_matrix, plot_confusion_matrix
+import matplotlib.pyplot as plt
+from matplotlib import figure
 #import data set
 
 dataset = pd.read_csv('Resume.csv')
 
 
+# some data visualization 
+import seaborn as sns
 
+sns.countplot(y="Category", data=dataset)
+
+count = dataset['Category'].value_counts()
 
 def cleanResume(resumeText):
     resumeText = resumeText.lower()
@@ -60,6 +67,24 @@ X_train,X_test,y_train,y_test = train_test_split(WordFeatures,requiredTarget,ran
 clf = OneVsRestClassifier(KNeighborsClassifier())
 clf.fit(X_train, y_train)
 prediction = clf.predict(X_test)
+#accuracy
+accuracy = accuracy_score(y_test, prediction, normalize=False)
+#some confusin matrix visualization
+
+cm = confusion_matrix(prediction, y_test)
+
+
+matrix = plot_confusion_matrix(clf, X_test, y_test, cmap=plt.cm.Reds )
+matrix.ax_.set_title("confusion matrix", color='black')
+plt.xlabel('predicted Label', color='black')
+plt.ylabel('True Label', color='black')
+plt.gcf().axes[0].tick_params(color='black')
+plt.gcf().axes[1].tick_params(color='black')
+plt.gcf().set_size_inches(10,6)
+plt.show()
+
+#display the output
+
 prediction = le.inverse_transform(prediction) # this will convert the encoder back to its original label 
 
 resume = docx2txt.process('resume.docx')
@@ -67,4 +92,5 @@ classify = clf.predict(word_vectorizer.transform([resume]))
 classify = le.inverse_transform(classify)
 
 print("Resume belongs to: ", classify[0])
+
 
